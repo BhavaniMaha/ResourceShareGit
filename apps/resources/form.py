c325374da5af51a35d129410bbd720d143139eb8
Bhavani M <bhavani.lingaraju@gmail.com>
2023-09-21 13:48:45 +0200

    Added database services

--- apps/resources/form.py
+++ apps/resources/form.py
@@ -1,5 +1,10 @@
 from django import forms
 
+# CATEGORIES = [
+#     ('1', 'Programming Language'),
+#     ('2', 'Databases'),
+# ]
+
 class PostResourceForm(forms.Form): 
     title = forms.CharField(widget=forms
                             .TextInput(attrs={
